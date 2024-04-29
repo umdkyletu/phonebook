b182e3d8cc524f988879a4337b92c69aa4ec5270
Caroline Mackin <cmackin712@gmail.com>
2024-04-29 18:03:37 -0400

    login connected to sql

--- FlaskApp/Website/auth.py
+++ FlaskApp/Website/auth.py
@@ -9,9 +9,19 @@ auth = Blueprint("auth", __name__)
 
 # whenever you go to / the stuff in home def will happen/appear
 @auth.route("/login", methods=['GET', 'POST'])
-def login():
-    data = request.form
-    print(data)
+def login():    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in sucessfully!', category='success')
+            else:
+                flash('Incorrect password. Please try again.', category='error')
+        else: 
+            flash('Email does not exist.', category='error')
     return render_template("login.html")
 
 
@@ -28,7 +38,10 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already registered.', category='error')
+        elif len(email) < 4:
             flash('Email must contain at least 4 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must contain at least 2 characters.', category='error')
--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 #runs server if main is run, debug = true means that it reruns flask website everytime code is changed
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000,debug=True)
