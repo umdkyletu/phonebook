ec8c097e5d8137541f09f5e2ac8a73c4b1d6da35
Kyle Tu <kyletu@umd.edu>
2024-05-07 22:05:20 -0400

    Added feature to add photos to contacts

--- FlaskApp/Website/models.py
+++ FlaskApp/Website/models.py
@@ -2,23 +2,20 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
     number = db.Column(db.String(150))
-    date = db.Column(db.DateTime(timezone = True),default = func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    photo = db.Column(db.String(150))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(150), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    contacts = db.relationship('Contact')
-
-
-
-
-
+    contacts = db.relationship("Contact")
--- FlaskApp/Website/views.py
+++ FlaskApp/Website/views.py
@@ -5,55 +5,64 @@ from . import db
 import json
 
 
-
 # define a blueprint for flask application
 views = Blueprint("views", __name__)
 
 
-@views.route("/",methods=['GET', 'POST'])
+@views.route("/", methods=["GET", "POST"])
 @login_required
 def contactlist():
-    if request.method == 'POST':
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        number = request.form.get('number')
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        number = request.form.get("number")
+        photo = request.form.get("photo")
         if len(first_name) < 2:
-            flash('First name must contain at least 2 characters.', category='error')
+            flash("First name must contain at least 2 characters.", category="error")
         if len(last_name) < 2:
-            flash('First name must contain at least 2 characters.', category='error')
+            flash("First name must contain at least 2 characters.", category="error")
         else:
-            new_contact = Contact(first_name = first_name, last_name=last_name, number=number, user_id = current_user.id)
+            new_contact = Contact(
+                first_name=first_name,
+                last_name=last_name,
+                number=number,
+                user_id=current_user.id,
+                photo=photo,
+            )
             db.session.add(new_contact)
             db.session.commit()
-            flash("Contact Added", category= 'success')
-            
-    return render_template("contactlist.html", user = current_user)
+            flash("Contact Added", category="success")
 
-@views.route('/delete-contact',methods=['POST'])
+    return render_template("contactlist.html", user=current_user)
+
+
+@views.route("/delete-contact", methods=["POST"])
 def delete_contact():
     contact = json.loads(request.data)
-    contactId = contact['contactId']
+    contactId = contact["contactId"]
     contact = Contact.query.get(contactId)
     if contact:
         if contact.user_id == current_user.id:
             db.session.delete(contact)
             db.session.commit()
-    
+
     return jsonify({})
 
-@views.route('/update/<int:id>', methods=['GET','POST'])
+
+@views.route("/update/<int:id>", methods=["GET", "POST"])
 def update(id):
     contact = Contact.query.get(id)
-    if request.method == 'POST':
-        contact.first_name = request.form.get('first_name')
-        contact.last_name = request.form.get('last_name')
-        contact.number = request.form.get('number')
-        print("First Name:",  contact.first_name)
+    if request.method == "POST":
+        contact.first_name = request.form.get("first_name")
+        contact.last_name = request.form.get("last_name")
+        contact.number = request.form.get("number")
+        contact.photo = request.form.get("photo")
+        print("First Name:", contact.first_name)
         print("Last Name:", contact.last_name)
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect("/")
         except:
             return "There was a problem updating"
     else:
-        return render_template('update.html',contact=contact, user = current_user)
+        return render_template("update.html", contact=contact, user=current_user)
