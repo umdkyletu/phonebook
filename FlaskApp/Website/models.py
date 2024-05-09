4b8e2e5d937ea679f6a290b4f49101f3a04ad4b8
Shaina Silawan <shainasilawan@gmail.com>
2024-05-09 12:41:07 -0400

    added docstrings

--- FlaskApp/Website/models.py
+++ FlaskApp/Website/models.py
@@ -4,6 +4,18 @@ from sqlalchemy.sql import func
 
 
 class Contact(db.Model):
+    """
+    Represents contact in database.
+    
+    Attr:
+        - id (int): primary key
+        - first_name (str): first name 
+        - last_name (str): last name
+        - number (str): phone number
+        - photo (str): url of photo representing contact
+        - date (DateTime): date and time when contact was added
+        - user_id (int):  foreign key referencing id of user who owns contact
+    """
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
@@ -14,6 +26,16 @@ class Contact(db.Model):
 
 
 class User(db.Model, UserMixin):
+    """
+    represents user in database.
+    
+    Attr:
+        - id (int): primary id
+        - email (str) email address
+        - password (str): password
+        - first_name (str): first name
+        - contacts (relationship): one-to-many rel with contact model
+    """
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
