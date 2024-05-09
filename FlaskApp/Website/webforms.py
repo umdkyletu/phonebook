a264e10cc868495522e12e85967da57c8f492729
Shaina Silawan <shainasilawan@gmail.com>
2024-05-09 12:41:42 -0400

    added docstrings

--- FlaskApp/Website/webforms.py
+++ FlaskApp/Website/webforms.py
@@ -4,5 +4,12 @@ from wtforms.validators import DataRequired
 
 #create a search form
 class SearchForm(FlaskForm):
+    """
+    form used for searching contacts.
+    
+    Attr:
+        - searched (stringField): field for entering search query.
+        - submit (SubmitField): button for submitting search query.
+    """
     searched = StringField("Searched", validators=[DataRequired()])
     submit = SubmitField("Submit")
